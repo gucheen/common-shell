b3ac6c79d4f2c869d3270838b444ee174d73af43
gucheng <guchengf@gmail.com>
2016-05-18 09:48:08 +0800

    Fix: command not found: nvm

--- node-env/install-node-env-nvm.sh
+++ node-env/install-node-env-nvm.sh
@@ -3,6 +3,9 @@
 # install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 
+# source nvm script
+. ~/.nvm/nvm.sh
+
 # install latest node
 nvm install node
 
--- node-env/nvm-install-latest.sh
+++ node-env/nvm-install-latest.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
+. ~/.nvm/nvm.sh
+
 NVM_NODEJS_ORG_MIRROR=http://npm.taobao.org/mirrors/node nvm install node --reinstall-packages-from=node
